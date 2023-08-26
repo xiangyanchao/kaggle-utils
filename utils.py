d9ed0b5427e78e827640469f2ddfb6cd22806486
xiangyanchao <1991858028@qq.com>
2023-08-26 22:41:29 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,19 @@
+import os
+os.environ["KERAS_BACKEND"] = "jax"
+
+import jax
+import numpy as np
+import tensorflow as tf
+import keras_core as keras
+
+from jax.experimental import mesh_utils
+from jax.sharding import Mesh
+from jax.sharding import NamedSharding
+from jax.sharding import PartitionSpec
+
+def get_devices():
+    num_devices = len(jax.local_devices())
+    print(f"Running on {num_devices} devices: {jax.local_devices()}")
+    devices = mesh_utils.create_device_mesh((num_devices,))
+    return devices
 
